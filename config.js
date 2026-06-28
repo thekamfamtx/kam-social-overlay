8537600f6be77bab20258250e63586e3140c609f
thekamfamtx <thekamfamtx@gmail.com>
2026-06-28 14:44:20 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+const CONFIG = {
+
+/* 📲 SOCIAL LINKS */
+social: {
+  facebook: {
+    url: "http://facebook.com/share/1ApkbDVWTA",
+    label: "FACEBOOK",
+    subtitle: "Join The KAM Fam Community",
+    color: "#1877F2"
+  },
+
+  instagram: {
+    url: "http://instagram.com/thekamfamtx",
+    label: "INSTAGRAM",
+    subtitle: "Behind the Scenes + Cruise Life",
+    color: "#dd2a7b"
+  },
+
+  youtube: {
+    url: "https://youtube.com/@TheKAMFamTX?sub_confirmation=1",
+    label: "YOUTUBE",
+    subtitle: "New Videos Every Week",
+    color: "#ff0000"
+  },
+
+  klub: {
+    url: "https://www.youtube.com/channel/UCPqBUO8PkVkOkoLjiV9rwsw/join",
+    label: "KAM KLUB",
+    subtitle: "Members Only Live Shows",
+    color: "#d4af37"
+  },
+
+  merch: {
+    url: "https://the-kam-fam-shop.fourthwall.com/",
+    label: "MERCH SHOP",
+    subtitle: "Grab your KAM Fam gear",
+    color: "#00c853"
+  }
+}
+
+};
